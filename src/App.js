8fa4837b795944dc0a8799098d5a0727704a296d
Steveen <steveen.echeverri@gmail.com>
2020-11-06 22:12:27 -0500

    Instance basic Navbar element

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import React from 'react'
+import { Navbar } from './components';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome</h1>        
+    <div>
+      <Navbar />        
     </div>
   );
 }
