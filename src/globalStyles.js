e258a2ea6a16a21d5851a41d507a6593965bbe25
Steveen <steveen.echeverri@gmail.com>
2020-11-11 19:15:23 -0500

    Completed add Navbar component and Router

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -25,4 +25,26 @@ padding-right: 50px;
 }
 `
 
+export const Button = styled.button`
+border-radius: 4px;
+background: ${({primary}) => (primary ? '#4B59F7' : '#0467FB')};
+white-space: nowrap;
+padding: ${({big}) => (big ? '12px 64px' : '10px 20px')};
+font-size: ${({fontBig}) => (fontBig ? '20px' : '17px')};
+outline: none;
+border: none;
+cursor: pointer;
+color: #fff;
+
+&:hover {
+    transition: all 0.3s ease-out;
+    background: #fff;
+    background: ${({primary}) => (primary ? '#0467FB' : '#4B59F7')};
+}
+
+@media screen and (max-width: 960px) {
+    width: 100%;
+}
+`
+
 export default GlobalStyle;
