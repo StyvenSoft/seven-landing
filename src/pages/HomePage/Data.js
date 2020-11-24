08fa6282e316861b93da7cb97941382b02ace508
Steveen <steveen.echeverri@gmail.com>
2020-11-23 23:05:57 -0500

    Created section Process plan white

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -10,6 +10,22 @@ export const homeObjOne = {
     description: 'We help bussiness owners increase their revenue. Our team of unique specialist can help you acheive your bussiness goals.',
     buttonLabel: 'Get Started',
     img: require('../../images/header.svg').default,
-    alt: '',
+    alt: 'Image development team',
     start: ''
+}
+
+export const homeObjTwo = {
+    lightBg: true,
+    imgStart: 'start',
+    lightTopLine: false,
+    lightText: false,
+    lightTextDesc: false,
+    primary: false,
+    topLine: 'Develop Process Plan',
+    headline: 'We will focus on technology so you can focus on growing your business',
+    description: 'Our team of developers will transform the designs into content-rich and interactive websites that are easy for visitors to use and easy for clients to maintain.',
+    buttonLabel: 'Know more',
+    img: require('../../images/photo-15.jpg').default,
+    alt: 'Image desktop computer',
+    start: 'true'
 }
