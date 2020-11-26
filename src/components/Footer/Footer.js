2ba3fad49c56e3ac5e3ab8657675bed1efb50a48
Steveen <steveen.echeverri@gmail.com>
2020-11-25 19:41:18 -0500

    Created section FooterLinksItems

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,18 @@
 import React from 'react';
-import { FooterContainer, FooterSubHeading, FooterSubscription, FooterSubText } from './Footer.elements';
+import { Button } from '../../globalStyles';
+import { 
+    FooterContainer, 
+    FooterSubHeading, 
+    FooterSubscription, 
+    FooterSubText, 
+    Form, 
+    FormInput,
+    FooterLinksContainer,
+    FooterLinksWrapper,
+    FooterLinksItems,
+    FooterLinkTitle,
+    FooterLink
+} from './Footer.elements';
 
 const Footer = () => {
     return (
@@ -11,7 +24,47 @@ const Footer = () => {
                 <FooterSubText>
                     You can unsubcribe at any
                 </FooterSubText>
+                <Form>
+                    <FormInput name='email' type='email' placeholder='Your Email' />
+                        <Button>Subscribe</Button>
+                </Form>
             </FooterSubscription>
+            <FooterLinksContainer>
+                <FooterLinksWrapper>
+                    <FooterLinksItems>
+                        <FooterLinkTitle>About us</FooterLinkTitle>
+                        <FooterLink to='/sign-up'>How it work</FooterLink>
+                        <FooterLink to='/'>Testimonials</FooterLink>
+                        <FooterLink to='/'>Careers</FooterLink>
+                        <FooterLink to='/'>Investors</FooterLink>
+                        <FooterLink to='/'>Terms of Services</FooterLink>
+                    </FooterLinksItems>
+                    <FooterLinksItems>
+                        <FooterLinkTitle>Contact us</FooterLinkTitle>
+                        <FooterLink to='/sign-up'>How it work</FooterLink>
+                        <FooterLink to='/'>Testimonials</FooterLink>
+                        <FooterLink to='/'>Careers</FooterLink>
+                        <FooterLink to='/'>Investors</FooterLink>
+                        <FooterLink to='/'>Terms of Services</FooterLink>
+                    </FooterLinksItems>
+                    <FooterLinksItems>
+                        <FooterLinkTitle>Videos</FooterLinkTitle>
+                        <FooterLink to='/sign-up'>How it work</FooterLink>
+                        <FooterLink to='/'>Testimonials</FooterLink>
+                        <FooterLink to='/'>Careers</FooterLink>
+                        <FooterLink to='/'>Investors</FooterLink>
+                        <FooterLink to='/'>Terms of Services</FooterLink>
+                    </FooterLinksItems>
+                    <FooterLinksItems>
+                        <FooterLinkTitle>Social Media</FooterLinkTitle>
+                        <FooterLink to='/sign-up'>How it work</FooterLink>
+                        <FooterLink to='/'>Testimonials</FooterLink>
+                        <FooterLink to='/'>Careers</FooterLink>
+                        <FooterLink to='/'>Investors</FooterLink>
+                        <FooterLink to='/'>Terms of Services</FooterLink>
+                    </FooterLinksItems>
+                </FooterLinksWrapper>
+            </FooterLinksContainer>
         </FooterContainer>
     )
 }
