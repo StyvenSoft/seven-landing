282bfe494b929606259023bd51ba6a7193a52724
Steveen <steveen.echeverri@gmail.com>
2020-11-26 21:49:13 -0500

    New component pricing

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { InfoSection } from '../../components'
+import { InfoSection, Pricing } from '../../components'
 import { homeObjOne, homeObjThree, homeObjTwo } from './Data'
 
 const Home = () => {
@@ -7,6 +7,7 @@ const Home = () => {
         <>
             <InfoSection {...homeObjOne} />
             <InfoSection {...homeObjTwo} />
+            <Pricing />
             <InfoSection {...homeObjThree} />
         </>
     )
